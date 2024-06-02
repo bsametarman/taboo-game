7616555c2d9e56852786ad01c51c44a6252545d5
bsametarman@outlook.com <bsametarman@outlook.com>
2024-06-02 22:44:01 +0300

    icon button added

--- lib/Materials/buttons.dart
+++ lib/Materials/buttons.dart
@@ -45,4 +45,26 @@ class Buttons {
       child: Text(text),
     );
   }
+
+  Widget iconButton(
+      {double radius = 5,
+      double width = 50,
+      double height = 50,
+      required Widget icon,
+      required VoidCallback onPressedFunction}) {
+    return IconButton(
+      icon: icon,
+      onPressed: onPressedFunction,
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(
+              AppTheme().lightTheme().colorScheme.inversePrimary),
+          minimumSize: MaterialStateProperty.all<Size>(Size(width, height)),
+          maximumSize:
+              MaterialStateProperty.all<Size>(Size(width + 50, height + 50)),
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+            RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(radius))),
+          )),
+    );
+  }
 }
