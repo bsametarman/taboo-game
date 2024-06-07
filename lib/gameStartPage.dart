eae843edd8be3bbb81ea98dfd11ea946569431cd
bsametarman@outlook.com <bsametarman@outlook.com>
2024-06-07 19:00:30 +0300

    some changes

--- lib/gameStartPage.dart
+++ lib/gameStartPage.dart
@@ -4,6 +4,7 @@ import 'package:tabu/Materials/buttons.dart';
 import 'package:tabu/Materials/sizedBoxes.dart';
 import 'package:tabu/Materials/textFields.dart';
 import 'package:tabu/gamePage.dart';
+import 'package:tabu/main.dart';
 
 class GameStartpage extends StatefulWidget {
   const GameStartpage({super.key});
@@ -21,6 +22,9 @@ class _GameStartpageState extends State<GameStartpage> {
     super.initState();
     _loadTeamAName();
     _loadTeamBName();
+    _setTeamAScore(0);
+    _setTeamBScore(0);
+    _setIsFirstTeam();
   }
 
   _loadTeamAName() async {
@@ -42,9 +46,24 @@ class _GameStartpageState extends State<GameStartpage> {
     });
   }
 
+  _setIsFirstTeam() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isFirstTeam', true);
+  }
+
   _saveTeamBNameValue(String value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString('teamBName', value);
+  }
+
+  _setTeamAScore(int score) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('teamAScore', 0);
+  }
+
+  _setTeamBScore(int score) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('teamBScore', 0);
   }
 
   @override
@@ -56,70 +75,76 @@ class _GameStartpageState extends State<GameStartpage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Container(
-          width: screenWidth * 0.8,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "1. Takım",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBoxes().verticalSizedBox(),
-              TextFields().textField(
-                text: "1. Takım Adı",
-                onChanged: (value) {
-                  setState(() {
-                    _teamAName = value;
-                    _saveTeamANameValue(value);
-                  });
-                },
-              ),
-              //Text(_teamAName),
-              SizedBoxes().verticalSizedBox(),
-              const Text(
-                "2. Takım",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBoxes().verticalSizedBox(),
-              TextFields().textField(
-                text: "2. Takım Adı",
-                onChanged: (value) {
-                  setState(() {
-                    _teamBName = value;
-                    _saveTeamBNameValue(value);
-                  });
-                },
-              ),
-              //Text(_teamBName),
-              SizedBoxes().verticalSizedBox(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Buttons().smallButton(
-                      text: "Geri",
-                      height: 50,
-                      width: 50,
-                      radius: 5,
-                      onPressedFunction: () {
-                        Navigator.pop(context);
-                      }),
-                  SizedBoxes().horizontalSizedBox(),
-                  Buttons().smallButton(
-                      text: "Başla",
-                      height: 50,
-                      width: 50,
-                      radius: 5,
-                      onPressedFunction: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const GamePage()));
-                      }),
-                ],
-              )
-            ],
+      body: PopScope(
+        canPop: false,
+        child: Center(
+          child: Container(
+            width: screenWidth * 0.8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "1. Takım",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBoxes().verticalSizedBox(),
+                TextFields().textField(
+                  text: "1. Takım Adı",
+                  onChanged: (value) {
+                    setState(() {
+                      _teamAName = value;
+                      _saveTeamANameValue(value);
+                    });
+                  },
+                ),
+                //Text(_teamAName),
+                SizedBoxes().verticalSizedBox(),
+                const Text(
+                  "2. Takım",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBoxes().verticalSizedBox(),
+                TextFields().textField(
+                  text: "2. Takım Adı",
+                  onChanged: (value) {
+                    setState(() {
+                      _teamBName = value;
+                      _saveTeamBNameValue(value);
+                    });
+                  },
+                ),
+                //Text(_teamBName),
+                SizedBoxes().verticalSizedBox(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Buttons().smallButton(
+                        text: "Geri",
+                        height: 50,
+                        width: 50,
+                        radius: 5,
+                        onPressedFunction: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MyApp()));
+                        }),
+                    SizedBoxes().horizontalSizedBox(),
+                    Buttons().smallButton(
+                        text: "Başla",
+                        height: 50,
+                        width: 50,
+                        radius: 5,
+                        onPressedFunction: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const GamePage()));
+                        }),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
